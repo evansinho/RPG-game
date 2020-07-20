e9e821a7d2c5a7e96c15817ad78654b73c5c3dc9
evansinho <igiri.evanson@gmail.com>
2020-07-20 11:09:56 +0100

    add a boot scene and load game assets

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
+/* eslint-disable no-undef */
 import 'phaser';
 import config from './config/config';
+import BootScene from './Scenes/BootScene';
+import PreloaderScene from './Scenes/PreloaderScene';
 
 class Game extends Phaser.Game {
   constructor() {
     super(config);
+    this.scene.add('Boot', BootScene);
+    this.scene.add('Preloader', PreloaderScene);
+    this.scene.start('Boot');
   }
 }
 
