ce98913d63d4c5ff978449733fc19eb22ff5b218
evansinho <igiri.evanson@gmail.com>
2020-07-20 14:12:57 +0100

    add title scene with buttons and bg music

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 /* eslint-disable no-undef */
 import 'phaser';
 import config from './config/config';
+import Music from './object/music';
 import BootScene from './Scenes/BootScene';
 import PreloaderScene from './Scenes/PreloaderScene';
+import TitleScene from './Scenes/TitleScene';
 
 class Game extends Phaser.Game {
   constructor() {
     super(config);
+    const music = new Music();
+    this.globals = { music, bgMusic: null };
     this.scene.add('Boot', BootScene);
     this.scene.add('Preloader', PreloaderScene);
+    this.scene.add('Title', TitleScene);
     this.scene.start('Boot');
   }
 }
